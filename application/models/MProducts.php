23c4a126e0a4eea4db76d9c7a62aa55d0bdbcb6e
Jose <solorzano202009@gmail.com>
2018-06-05 12:13:51 -0400

    Actualización de la base de datos.

--- application/models/MProducts.php
+++ application/models/MProducts.php
@@ -9,12 +9,14 @@ class MProducts extends CI_Model {
     public function __construct() {
 
         parent::__construct();
-        $this->db_psadmin = $this->load->database('psadmin', TRUE);  // Indicamos que use la base de datos 'psadmin' en vez de 'm32018'
+        $this->load->database();
+        //~ $this->db_psadmin = $this->load->database('psadmin', TRUE);  // Indicamos que use la base de datos 'psadmin' en vez de 'm32018'
     }
 
     //Public method to obtain the orders
     public function obtener() {
-        $query = $db_psadmin->get('attribute_product');
+        $query = $this->db->get('attribute_product');
+        //~ $query = $db_psadmin->get('attribute_product');
 
         if ($query->num_rows() > 0)
             return $query->result();
@@ -26,14 +28,22 @@ class MProducts extends CI_Model {
     public function obtenerById($table, $field, $value) {
 		
 		$select = "a_g_l.id_attribute_group, a_g_l.public_name, a_l.id_attribute, a_l.name";
-		$query = $this->db_psadmin->select($select);
-		$query = $this->db_psadmin->from($table);
-		$query = $this->db_psadmin->join('attribute_group_lang a_g_l', 'a_p.id_attribute_group=a_g_l.id_attribute_group');
-		$query = $this->db_psadmin->join('attribute_lang a_l', 'a_p.id_attribute_lang=a_l.id_attribute');
-        $query = $this->db_psadmin->where($field, $value);
-        $query = $this->db_psadmin->group_by(array('a_p.id_attribute_group', 'a_l.name'));
-        $query = $this->db_psadmin->order_by('a_l.id_attribute');
-        $query = $this->db_psadmin->get();
+		$query = $this->db->select($select);
+		$query = $this->db->from($table);
+		$query = $this->db->join('attribute_group_lang a_g_l', 'a_p.id_attribute_group=a_g_l.id_attribute_group');
+		$query = $this->db->join('attribute_lang a_l', 'a_p.id_attribute_lang=a_l.id_attribute');
+        $query = $this->db->where($field, $value);
+        $query = $this->db->group_by(array('a_p.id_attribute_group', 'a_l.name'));
+        $query = $this->db->order_by('a_l.id_attribute');
+        $query = $this->db->get();
+		//~ $query = $this->db_psadmin->select($select);
+		//~ $query = $this->db_psadmin->from($table);
+		//~ $query = $this->db_psadmin->join('attribute_group_lang a_g_l', 'a_p.id_attribute_group=a_g_l.id_attribute_group');
+		//~ $query = $this->db_psadmin->join('attribute_lang a_l', 'a_p.id_attribute_lang=a_l.id_attribute');
+        //~ $query = $this->db_psadmin->where($field, $value);
+        //~ $query = $this->db_psadmin->group_by(array('a_p.id_attribute_group', 'a_l.name'));
+        //~ $query = $this->db_psadmin->order_by('a_l.id_attribute');
+        //~ $query = $this->db_psadmin->get();
         
         //~ echo $db_psadmin->last_query();
 
