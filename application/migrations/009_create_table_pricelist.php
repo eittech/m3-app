8ca053212a9415c5e5159c2e0cfc3f3602b725cb
Jose <solorzano202009@gmail.com>
2018-07-20 17:00:22 -0400

    Nuevo archivo de migración y borrado de archivos assets innecesarios.

---
+++ application/migrations/009_create_table_pricelist.php
@@ -0,0 +1,88 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_create_table_pricelist extends CI_Migration
+{
+	public function up(){
+		
+		// Creamos la estructura de la nueva tabla usando la clase dbforge de Codeigniter
+		$this->dbforge->add_field(
+			array(
+				"id" => array(
+					"type" => "INT",
+					"constraint" => 11,
+					"unsigned" => TRUE,
+					"auto_increment" => TRUE,
+					"null" => FALSE
+				),
+				"list_number" => array(
+					"type" => "INT",
+					"constraint" => 11,
+					"null" => TRUE
+				),
+				"date" => array(
+					"type" => "TIMESTAMP",
+					"null" => TRUE
+				),
+				"position" => array(
+					"type" => "INT",
+					"constraint" => 11,
+					"null" => TRUE
+				),
+				"category" => array(
+					"type" => "VARCHAR",
+					"constraint" => 100,
+					"null" => FALSE
+				),
+				"subcategory" => array(
+					"type" => "VARCHAR",
+					"constraint" => 200
+				),
+				"reference" => array(
+					"type" => "VARCHAR",
+					"constraint" => 100
+				),
+				"product" => array(
+					"type" => "VARCHAR",
+					"constraint" => 100,
+					"null" => TRUE
+				),
+				"id_combination" => array(
+					"type" => "INT",
+					"constraint" => 11,
+					"null" => TRUE
+				),
+				"combination" => array(
+					"type" => "VARCHAR",
+					"constraint" => 100,
+					"null" => TRUE
+				)
+				"price_cost" => array(
+					"type" => "FLOAT",
+					"null" => TRUE
+				),
+				"price_wholesaler" => array(
+					"type" => "FLOAT",
+					"null" => TRUE
+				),
+				"price_retail" => array(
+					"type" => "FLOAT",
+					"null" => TRUE
+				)
+			)
+		);
+		
+		$this->dbforge->add_key('id', TRUE);  // Establecemos el id como primary_key
+		
+		$this->dbforge->create_table('pricelist', TRUE);
+		
+	}
+	
+	public function down(){
+		
+		// Eliminamos la tabla 'pricelist'
+		$this->dbforge->drop_table('pricelist', TRUE);
+		
+	}
+	
+}
